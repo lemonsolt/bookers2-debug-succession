e040d5cac2be57cb627f0f9f6601206eae18f871
SaraKinoshita <135306445+lemonsolt@users.noreply.github.com>
2023-07-05 06:52:12 +0000

    [update]

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :book
+  # そのユーザーidにおいて、そのブックidには一度しか押せないよの意
+  # つまり１冊に対し一人１ふぁぼの制限を付ける分
+  validates_uniqueness_of :book_id, scope: :user_id
 end
